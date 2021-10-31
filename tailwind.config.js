a967a08fec93ad43840ee1e20837e432ff61ad6b
jay <jaeykweon@naver.com>
2021-11-01 01:00:43 +0900

    [add] 세팅 변경

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+const defaultTheme  = require('tailwindcss/defaultTheme')
+const colors  = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+      black: colors.black
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+        '300': '75rem'
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  variants: {
+    extend: {
+      borderColor: ['focus-visible'],
+      opacity: ['disabled'],
+    }
+  }
+}
