7b7c4c9e0098d1e71609a0cf80e286613ea57f8d
jay <jaeykweon@naver.com>
2021-11-04 00:26:28 +0900

    [change] 홀수 짝수로 나눠서 배치

--- components/BookContainer.vue
+++ components/BookContainer.vue
@@ -2,13 +2,22 @@
     <section class="lg:px-20 md:px-10 px-5z min-w-full flex justify-center ">
 
             <ul class="sm:w-144 inline-flex flex-wrap justify-items-center justify-between">
-                
-                <li v-for="book in books" :key="book.id" class="sm:w-64 w-40 px-2 py-2 text-center" >
-                    <nuxt-link :to="{path: '/detail'}">
-                        <img :src="book.img_src" />
-                    </nuxt-link>
-                </li>
-                
+
+                <div class="w-1/2 pr-2">
+                    <li v-for="oddIdBook in sortedBooks[0]" :key="oddIdBook.id" class="sm:w-64 w-40  py-3 text-center" >
+                        <nuxt-link :to="{path: '/detail'}">
+                            <img :src="oddIdBook.img_src" />
+                        </nuxt-link>
+                    </li>
+                    
+                </div>
+                <div class="w-1/2 pl-2">
+                    <li v-for="evenIdBook in sortedBooks[1]" :key="evenIdBook.id" class="sm:w-64 w-40  py-3 text-center" >
+                        <nuxt-link :to="{path: '/detail'}">
+                            <img :src="evenIdBook.img_src" />
+                        </nuxt-link>
+                    </li>
+                </div>
             </ul>
 
     </section>
@@ -24,6 +33,16 @@ export default {
     data(){
         return {
             books : bookData
+        }
+    },
+    computed: {
+        sortedBooks() {
+            const sortedBooks = [[],[]];
+            for (let i=0; i<this.books.length; i++){
+                if(i%2 === 0) {sortedBooks[0].push(this.books[i])}
+                else {sortedBooks[1].push(this.books[i])}
+            }
+            return sortedBooks
         }
     }
 }
