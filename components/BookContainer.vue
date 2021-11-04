86df213363b4b2c91f283580dadb2b1b4cf14bdb
jay <jaeykweon@naver.com>
2021-11-05 00:51:56 +0900

    [upgrade] 각 사진 중앙 정렬, 경계선 추가, 간격 수정

--- components/BookContainer.vue
+++ components/BookContainer.vue
@@ -1,25 +1,21 @@
 <template>
-    <section class="lg:px-20 md:px-10 px-5z min-w-full flex justify-center ">
-
-            <ul class="sm:w-144 inline-flex flex-wrap justify-items-center justify-between">
-
-                <div class="w-1/2 pr-2">
-                    <li v-for="oddIdBook in sortedBooks[0]" :key="oddIdBook.id" class="sm:w-64 w-40  py-3 text-center" >
-                        <nuxt-link :to="{path: '/detail'}">
-                            <img :src="oddIdBook.img_src" />
+    <section class="lg:px-20 md:px-10 min-w-full flex justify-center ">
+            <ul class="sm:w-144 min-w-full px-2 inline-flex flex-wrap justify-items-center justify-between">
+                <div class="w-1/2 pl-2 border-l-2 block">
+                    <li v-for="oddIdBook in sortedBooks[0]" :key="oddIdBook.id" class="sm:w-64 w-36 py-3 flex m-auto justify-center" >
+                        <nuxt-link :to="{path: `/detail?book_id=${oddIdBook.id}`}">
+                                <img :src="oddIdBook.img_src" />
                         </nuxt-link>
                     </li>
-                    
                 </div>
-                <div class="w-1/2 pl-2">
-                    <li v-for="evenIdBook in sortedBooks[1]" :key="evenIdBook.id" class="sm:w-64 w-40  py-3 text-center" >
-                        <nuxt-link :to="{path: '/detail'}">
+                <div class="w-1/2 pr-2 border-r-2">
+                    <li v-for="evenIdBook in sortedBooks[1]" :key="evenIdBook.id" class="sm:w-64 w-36 py-3 flex m-auto justify-center" >
+                        <nuxt-link :to="{path: `/detail?book_id=${evenIdBook.id}`}">
                             <img :src="evenIdBook.img_src" />
                         </nuxt-link>
                     </li>
                 </div>
             </ul>
-
     </section>
 </template>
 
