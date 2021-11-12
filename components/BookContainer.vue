9b193a30fc7f6b85e9ed17d6cb6990d9593f158e
jay <jaeykweon@naver.com>
2021-11-13 01:35:31 +0900

    [fix] 이미지 크기 조절 및 중앙 정렬

--- components/BookContainer.vue
+++ components/BookContainer.vue
@@ -32,16 +32,16 @@
     <section class="lg:px-20 md:px-10 px-2 min-w-full flex justify-center ">
             <ul class="sm:w-144 min-w-full border-b-2 inline-flex flex-wrap justify-items-center justify-center">
                 <div class="w-full px-2 border-l-2 border-r-2 m-auto inline justify-around">
-                    <li v-for="book in sortedBooks" :key="book.id" class="sm:w-64 w-36 py-3 mx-2 inline-block justify-center" >
-                        <div class="text-left">
+                    <li v-for="book in sortedBooks" :key="book.id" class="sm:w-64 w-36 py-3 mx-2 inline-block" >
+                        <div class="text-left inline-block">
                             <!-- https://web2tailwind.com/component/badge -->
                             <span v-if="book.state.new" class="inline-block bg-blue-600 text-gray-200 text-xs px-2 py-1 rounded-full">신규</span>
                             <span v-if="book.state.on_air" class="inline-block bg-gray-600 text-gray-100 text-xs px-2 py-1 rounded-full">대출</span>
                             <span v-if="book.state.finished" class="inline-block bg-green-400 text-gray-100 text-xs px-2 py-1 rounded-full">읽음</span>
                         </div>
-                        <div class="w-36 h-52">
+                        <div class="w-36 h-52 flex flex-wrap content-center">
                             <nuxt-link :to="{path: '/detail', query:{ book_id:`${book.id}`}}">
-                                <img class="transform transition motion-reduce:transform-none hover:scale-110 rounded-md duration-50 pt-1 filter drop-shadow-xl" :src="book.img_src" />
+                                    <img class="transform transition motion-reduce:transform-none hover:scale-110 rounded-md duration-50 max-h-52 pt-1 filter drop-shadow-xl" :src="book.img_src" />
                             </nuxt-link>
                         </div>
                     </li>
