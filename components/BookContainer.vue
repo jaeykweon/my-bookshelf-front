6b73c50175d8410f2275f71b6641a6da2c91b0a1
jay <jaeykweon@naver.com>
2021-11-14 23:43:36 +0900

    [new] category

--- components/BookContainer.vue
+++ components/BookContainer.vue
@@ -1,51 +1,34 @@
 <template>
-    <!-- <section class="lg:px-20 md:px-10 px-2 min-w-full flex justify-center ">
-            <ul class="sm:w-144 min-w-full border-b-2 inline-flex flex-wrap justify-items-center justify-between">
-                <div class="w-1/2 pl-2 border-l-2 block">
-                    <li v-for="oddIdBook in paginatedBooks[0]" :key="oddIdBook.id" class="sm:w-64 w-36 py-3 block m-auto justify-center" >
-                        <div class="">
-                            https://web2tailwind.com/component/badge 
-                            <span v-if="oddIdBook.state.new" class="inline-block bg-blue-600 text-gray-200 text-xs px-2 py-1 rounded-full">신규</span>
-                            <span v-if="oddIdBook.state.on_air" class="inline-block bg-gray-600 text-gray-100 text-xs px-2 py-1 rounded-full">대출</span>
-                            <span v-if="oddIdBook.state.finished" class="inline-block bg-green-400 text-gray-100 text-xs px-2 py-1 rounded-full">읽음</span>
-                        </div>
-                        <nuxt-link :to="{path: '/detail', query:{ book_id:`${oddIdBook.id}`}}">
-                            <img class="w-36 h-52 filter drop-shadow-xl pt-1 " :src="oddIdBook.img_src" />
-                        </nuxt-link>
-                    </li>
-                </div>
-                <div class="w-1/2 pr-2 border-r-2">
-                    <li v-for="evenIdBook in paginatedBooks[1]" :key="evenIdBook.id" class="sm:w-64 w-36 py-3 block m-auto justify-center" >
-                        <div class="inline">
-                            <div v-if="evenIdBook.state.new" class="inline-block bg-blue-600 text-gray-200 text-xs px-2 py-1 rounded-full">신규</div>
-                            <div v-if="evenIdBook.state.on_air" class="inline-block bg-gray-600 text-gray-100 text-xs px-2 py-1 rounded-full">대출</div>
-                            <div v-if="evenIdBook.state.finished" class="inline-block bg-green-400 text-gray-100 text-xs px-2 py-1 rounded-full">읽음</div>
-                        </div>
-                        <nuxt-link :to="{path: `/detail?book_id=${evenIdBook.id}`}">
-                            <img class="w-36 h-52 filter drop-shadow-xl pt-1" :src="evenIdBook.img_src" />
-                        </nuxt-link>
-                    </li>
-                </div>
-            </ul>
-    </section> -->
+    <section class="lg:px-20 md:px-10 px-2 min-w-full justify-center">
+            <div class="min-w-full">
+                    <ul class="grid grid-cols-2 w-1/2">
+                        <li v-for="bookCategory in bookCategories" :key="bookCategory" class="flex flex-wrap items-center">
+                            <input 
+                                v-model="selectedCategoryList" 
+                                :value="bookCategory" 
+                                class="text-indigo-500 w-5 h-5 mr-2 focus:ring-indigo-400 focus:ring-opacity-25 border border-gray-300 rounded" 
+                                type="checkbox" 
+                                @change="addCategoryToParam()"
+                            />
+                            <label>{{ bookCategory }}</label>
+                        </li>
+                    </ul>
+            </div>
 
-    <section class="lg:px-20 md:px-10 px-2 min-w-full justify-center ">
-            <ul class="sm:w-144 min-w-full border-b-2 flex flex-wrap justify-items-center justify-center">
-                <div class="w-full px-2 border-l-2 border-r-2 m-auto grid justify-items-center xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2">
-                    <li v-for="book in sortedBooks" :key="book.id" class="sm:w-44 w-36 py-3 mx-2 inline-grid text-center justify-center" >
-                        <div class="text-left inline-block">
-                            <!-- https://web2tailwind.com/component/badge -->
-                            <span v-if="book.state.new" class="inline-block bg-blue-600 text-gray-200 text-xs px-2 py-1 rounded-full">신규</span>
-                            <span v-if="book.state.on_air" class="inline-block bg-gray-600 text-gray-100 text-xs px-2 py-1 rounded-full">대출</span>
-                            <span v-if="book.state.finished" class="inline-block bg-green-400 text-gray-100 text-xs px-2 py-1 rounded-full">읽음</span>
-                        </div>
-                        <div class="w-36 h-52 flex flex-wrap content-center">
-                            <nuxt-link :to="{path: '/detail', query:{ book_id:`${book.id}`}}">
-                                    <img class="transform transition motion-reduce:transform-none hover:scale-110 rounded-md duration-50 max-h-52 pt-1 filter drop-shadow-xl" :src="book.img_src" />
-                            </nuxt-link>
-                        </div>
-                    </li>
-                </div>
+            <ul class="w-full px-2 border-2 m-auto grid justify-items-center 2xl:grid-cols-7 xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2">
+                <li v-for="book in sortedBooks" :key="book.id" class="sm:w-44 w-36 py-3 mx-2 inline-grid text-center justify-center" >
+                    <div class="text-left inline-block">
+                        <!-- https://web2tailwind.com/component/badge -->
+                        <span v-if="book.state.new" class="inline-block bg-blue-600 text-gray-200 text-xs px-2 py-1 rounded-full">신규</span>
+                        <span v-if="book.state.on_air" class="inline-block bg-gray-600 text-gray-100 text-xs px-2 py-1 rounded-full">대출</span>
+                        <span v-if="book.state.finished" class="inline-block bg-green-400 text-gray-100 text-xs px-2 py-1 rounded-full">읽음</span>
+                    </div>
+                    <div class="w-36 h-52 flex flex-wrap content-center">
+                        <nuxt-link :to="{path: '/detail', query:{ book_id:`${book.id}`}}">
+                                <img class="transform transition motion-reduce:transform-none hover:scale-110 rounded-md duration-50 max-h-52 pt-1 filter drop-shadow-xl" :src="book.img_src" />
+                        </nuxt-link>
+                    </div>
+                </li>
             </ul>
     </section>
 </template>
@@ -54,25 +37,36 @@
 <script>
 
 import bookData from 'static/.test/books.json';
+import bookCategories from 'static/.test/bookCategories.json';
 
 export default {
     name: 'App',
     data(){
+        const selectedCategoryList = [];
+        if (typeof this.$route.query.category === "string"){
+            for (const each of this.$route.query.category.split(',')){
+                selectedCategoryList.push(each);
+            }
+        }else if(this.$route.query.category){
+            for (const each of this.$route.query.category){
+                selectedCategoryList.push(each);
+            }
+        }
         return {
             page : this.$route.query.page || 1,
             books : bookData,
-            categoryList : typeof this.$route.query.category === "string"? this.$route.query.category.split(','): this.$route.query.category,
+            bookCategories,
+            selectedCategoryList,
             stateList : typeof this.$route.query.state === "string"? this.$route.query.state.split(',') : this.$route.query.state
         }
     },
     computed: {
-
         sortedBooks(){
             const sortedBooks = [];
+            console.log("this.selectedCategoryList", this.selectedCategoryList);
             for (const eachBook of this.books){
-                console.log("this.categoryList", this.categoryList);
                 
-                if(this.categoryList && !this.categoryList?.includes(eachBook.category.main)) {  // this.categoryList && 
+                if(this.selectedCategoryList?.length > 0 && !this.selectedCategoryList?.includes(eachBook.category.main)) {  // this.categoryList && 
                     // 카테고리 조건으로 검색했고 그 검색어에 없는 경우
                     continue
                 };
@@ -89,13 +83,26 @@ export default {
                         }
                     }
                 }
-                else{stateCheck = true}
-                
+                else{
+                    stateCheck = true
+                }
+
                 if(stateCheck){
                     sortedBooks.push(eachBook);
                 }
             }
             return sortedBooks
+        },
+    },
+    watch:{
+        // '$route.query'(){
+        //     this.sortedBooks()
+        // }
+    },
+    methods:{
+        addCategoryToParam(){
+            console.log("this.selectedCategoryList", this.selectedCategoryList);
+            this.$router.push({query:{state: this.selectedCategoryList}})
         }
     }
 }
